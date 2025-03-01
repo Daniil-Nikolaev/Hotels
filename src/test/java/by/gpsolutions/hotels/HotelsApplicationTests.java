acf2697b3c76935092cc2e04c464907a5d366ad3
Daniil <daniillalala14@gmail.com>
2025-03-02 01:11:48 +0300

    initial commit

---
+++ src/test/java/by/gpsolutions/hotels/HotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gpsolutions.hotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
