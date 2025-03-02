2a4725965bae9b5cf7eec0689964518f6471a096
Daniil <daniillalala14@gmail.com>
2025-03-02 13:43:57 +0300

    Added entity&liquibase migration

---
+++ src/main/java/by/gpsolutions/hotels/entity/Hotel.java
@@ -0,0 +1,116 @@
+package by.gpsolutions.hotels.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    @Embedded
+    private Address address;
+    @Embedded
+    private Contacts contacts;
+    @Embedded
+    private ArrivalTime arrivalTime;
+    @ElementCollection
+    @CollectionTable(name = "hotel_amenities", joinColumns = @JoinColumn(name = "hotel_id"))
+    @Column(name = "amenity")
+    private List<String> amenities;
+
+    public Hotel(String name, String description, String brand, Address address, Contacts contacts, ArrivalTime arrivalTime, List<String> amenities) {
+        this.name = name;
+        this.description = description;
+        this.brand = brand;
+        this.address = address;
+        this.contacts = contacts;
+        this.arrivalTime = arrivalTime;
+        this.amenities = amenities;
+    }
+
+    public Hotel() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Contacts getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(Contacts contacts) {
+        this.contacts = contacts;
+    }
+
+    public ArrivalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(ArrivalTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public List<String> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(List<String> amenities) {
+        this.amenities = amenities;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", brand='" + brand + '\'' +
+                ", address=" + address +
+                ", contacts=" + contacts +
+                ", arrivalTime=" + arrivalTime +
+                ", amenities=" + amenities +
+                '}';
+    }
+}
